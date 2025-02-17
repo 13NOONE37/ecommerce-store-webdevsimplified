1a4c430aeafb5b5b4cc765555b8ed68291315884
13NOONE37 <oliwerklauze22@gmail.com>
2025-02-17 22:18:04 +0100

    progress

--- src/app/admin/products/new/page.tsx
+++ src/app/admin/products/new/page.tsx
@@ -1,1 +1,11 @@
-export default function NewProductPage() {}
+import { PageHeader } from '../../_components/PageHeader';
+import { ProductForm } from '../_components/ProductForm';
+
+export default function NewProductPage() {
+  return (
+    <>
+      <PageHeader>Add Product</PageHeader>
+      <ProductForm />
+    </>
+  );
+}
